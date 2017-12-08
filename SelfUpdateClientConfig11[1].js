35d7c3bf1930dc84df4b52165489f50a40c02ebf
endz80513 <34360753+endz80513@users.noreply.github.com>
2017-12-07 19:40:56 -0800

    Add files via upload

---
+++ SelfUpdateClientConfig11[1].js
@@ -0,0 +1,26 @@
+{
+    "config": [
+        {
+            "config_app": "da_csp",
+            "version": "1.0.2.2028",
+            "appName": "Data Analytics CSP",
+            "checksum": "4343",
+            "download_path_primary": "http://www.zebra.com/content/dam/zebra/software/en/emc-analytics-update/DataAnalyticsCSP.apk",
+            "download_path_secondary": "https://s3-us-west-2.amazonaws.com/da-binaries/2.2/apk/com.symbol.mxmf.csp.analyticsmgr.apk"
+
+        },
+        {
+            "config_app": "da_engine",
+            "version": "1.0.2.2091",
+            "appName": "Data Analytics Engine",
+            "checksum": "1212",
+            "download_path_primary": "http://www.zebra.com/content/dam/zebra/software/en/emc-analytics-update/DataAnalyticsEngine.apk" ,
+            "download_path_secondary": "https://s3-us-west-2.amazonaws.com/da-binaries/2.2/apk/com.symbol.dataAnalyticsEngine.apk"
+        }
+
+    ],
+    "config_version": "1.0.2.53",
+    "poll_interval": "1440",
+    "config_path_URL": "http://api.zebra.com/v1/config/emc-analytics-update",
+    "config_path_secondary":"https://s3-us-west-2.amazonaws.com/da-binaries/2.2/config/emc-analytics-update"
+}
